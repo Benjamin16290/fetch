5794ca4e2286d0dc0fba7ee3a999f198cea01ea5
Benjamin16290 <carderonbenjamin16@gmail.com>
2024-09-03 10:04:38 +0200

    initial commit

--- Js/main.js
+++ Js/main.js
@@ -1,11 +1,5 @@
 
  
-
-
-
-
-
-
 const options = {
     method: 'GET',
     headers: {
@@ -14,58 +8,67 @@ const options = {
     }
   };
   
-  const List_movie= document.querySelector ('ul');
-  const article = document.querySelector ('article');
-  const inputmovie = document.querySelector ('inputmovie');
+ 
+  const List_movie = document.querySelector ('section');
+  const inputmovie = document.querySelector ('#inputmovie');         
+  let result = [];
+
+  inputmovie.addEventListener ('keyup', function (e) {
+    const search = e.target.value.toLowerCase ();
+     const newList = result.filter((movie) =>movie.title.toLowerCase().includes(search) )
+     
+    })
+   
   
   fetch('https://api.themoviedb.org/3/trending/movie/day?language=en-US', options)
     .then(response => response.json())
-    .then(response => { 
+    .then(response => { console.log (response)
       for (const movie of response.results) {
-          const li = document.createElement('li');
           const img = document.createElement('img');
-          li.textContent = movie.original_title;
+          const contain =document.createElement('article');
+          const title = document.createElement ('h2');
+          const over = document.createElement ('p');
+          const date = document.createElement ('p');
+          const vote = document.createElement ('p');
           img.src = "https://image.tmdb.org/t/p/original" + movie.poster_path;
-          List_movie.append(li, img); 
           
-           
-   const title = document.createElement ('h2');
-   const vote = document.createElement ('p');
-   const date = document.createElement ('p');
-   title.textContent = movie.title;
-   vote.textContent = movie.overview;
-   date.textContent = movie.release_date;
-   List_movie.append(title,vote,date,);
+          
+   title.textContent = movie.title;               
+   over.textContent =  movie.overview;
+   date.textContent = "Release: "+movie.release_date;
+   vote.textContent = "Vote: "+movie.vote_count;
+   contain.append(title,img,over,date,vote);
+   List_movie.appendChild(contain);
+
+   
 
    img.addEventListener('click', () => {
     if (vote.style.display === 'none') {
     vote.style.display = 'block';
     date.style.display = 'block';
+    over.style.display = 'block';
+    
    }
 
     else {
       vote.style.display = 'none';
       date.style.display = 'none';
+      over.style.display = 'none';
+     
     }
 
 
 
+
+
+})
+     } }
     
-});
-
-  
-     } })
-     
-     
-
-  
+    
+    )  
     .catch(err => console.error(err));
 
-
-
- 
-
-
-  
-   
     
+function createList () {
+  
+}
